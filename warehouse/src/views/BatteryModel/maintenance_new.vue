b2edb4fbefc9ba198c48ce609bc5dc7ff86aa014
pichead <61868523+pichead@users.noreply.github.com>
2022-04-26 17:01:31 +0700

    maintenance new batt create
    maintenance new batt create

--- warehouse/src/views/BatteryModel/maintenance_new.vue
+++ warehouse/src/views/BatteryModel/maintenance_new.vue
@@ -10,7 +10,7 @@
                     <div class="row">
                         <div class="col-4 col-form-label font-weight-bold">Inspection No.</div>
                         <div class="col">
-                            <select class="form-control" id=""></select>
+                            <select class="form-control" id="inspection_option"></select>
                         </div>
                     </div>
                 </div>
@@ -18,7 +18,7 @@
                     <div class="row">
                         <div class="col-4 pl-5 col-form-label font-weight-bold">Po No.</div>
                         <div class="col">
-                            <select class="form-control" id=""></select>
+                            <select class="form-control" id="po_option" disabled></select>
                         </div>
                     </div>
                 </div>
@@ -40,7 +40,7 @@
             </div>
         </div>
         <div class="p-4 border">
-            <table class="col table table-striped">
+            <table id="data_table" class="col table">
                 <colgroup>
                     <col span="1" style="width: 5%;">
                     <col span="1" style="width: 15%;">
@@ -51,7 +51,7 @@
                     <col span="1" style="width: 10%;">
                     <col span="1" style="width: 10%;">
                 </colgroup>
-                <thead class="text-center ">
+                <thead class="text-center thead-dark">
                     <tr class="">
                         <th>No.</th>
                         <th>Inspection</th>
@@ -63,8 +63,8 @@
                         <th>จำนวนExport</th>
                     </tr>
                 </thead>
-                <tbody class="text-center">
-                    <tr>
+                <tbody id="data" class="text-center">
+                    <!-- <tr>
                         <td >1</td>
                         <td>IF_30/2022</td>
                         <td>PEC056/22</td>
@@ -76,42 +76,7 @@
                             <input type="number" class="col" value="0">
                         </td>
                     </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>IF_29/2022</td>
-                        <td>PEC055/22</td>
-                        <td>65/034</td>
-                        <td>UPS12-490MRX</td>
-                        <td>โกดัง PEC</td>
-                        <td>100</td>
-                        <td>
-                            <input type="number" class="col" value="0">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>IF_28/2022</td>
-                        <td>PEC054/22</td>
-                        <td>65/000</td>
-                        <td>UPS12-490MRX</td>
-                        <td>โกดัง PEC</td>
-                        <td>100</td>
-                        <td>
-                            <input type="number" class="col" value="0">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>IF_27/2022</td>
-                        <td>PEC053/22</td>
-                        <td>65/000</td>
-                        <td>UPS12-490MRX</td>
-                        <td>โกดัง PEC</td>
-                        <td>100</td>
-                        <td>
-                            <input type="number" class="col" value="0">
-                        </td>
-                    </tr>
+       -->
                 </tbody>
             </table>
         </div>
@@ -135,17 +100,137 @@ export default {
     components: { Sidebar },
     mounted() {
         
-        var batt_data = []
-        preload()
+        page_setup()
+
+        function page_setup(){
+            $('#inspection_option').html(
+                '<option value="0" class="ins_option" selected>ทั้งหมด</option>'
+            )
+            $('#po_option').html(
+                '<option value="0" class="po_option" selected>ทั้งหมด</option>'
+            )
+            preload()
+        }
+        
+        
         async function preload(){
-            const batt = await projectFirestore.collection("Batteries_beta").get()
-            batt.forEach((data)=>{
-                batt.push(data.data())
-            })
+            var batt_data = []
+            var inspection = []
+            var inspection_data = []
+            var row_model_count = {}
+  
+
+            const batt = await projectFirestore.collection("Batteries_beta").where('measurements','==',[]).get()
+            
+            await setup()
+            await uniq_inspection()
+            await get_PO()
+            await render_predata()
+            await running_number()
+            await render_predata2()
+            await render_predata3()
+
+            function setup(){
+                
+                
+                batt.forEach((data)=>{
+                    batt_data.push(data.data())
+                    inspection.push(data.data().inspectionId)
+                })
+            }
+
+            
+            function uniq_inspection(){
+                inspection = Array.from(new Set(inspection))
+            }
+
+            async function get_PO(){
+                for(let i = 0; i < inspection.length; i++ ){
+                    const ins = await projectFirestore.collection("InspectionForm").doc(inspection[i]).get()
+                    inspection_data.push(ins.data())
+                }
+            }
+
+            function render_predata(){
+                 $('#data').html('')
+                for(let i = 0; i < inspection_data.length; i++){
+                    // console.log($('.ins_option').length)
+                    if($('.ins_option').length == 1){
+                        $('#inspection_option').append(
+                            '<option class="ins_option" value="'+inspection_data[i].inspection_no+'">'+inspection_data[i].inspection_no+'</option>'
+                        )
+                    }
+                    
+                    for(let j = 0; j < inspection_data[i].gen_barcode_data.length; j++ ){
+                        const key_batt = inspection_data[i].gen_barcode_data[j].ordersheetBatt
+                        
+                        row_model_count[key_batt] = 0;
+                        
+                        // row_model_count.push('{row:row_model_count,count:0}')
+                        $('#data').append(
+                            '<tr class="table_data" data-ins="'+inspection_data[i].inspection_no+'" data-po="'+inspection_data[i].gen_barcode_data[j].poNo+'">'+
+                                '<td class="row_number"></td>'+
+                                '<td>'+inspection_data[i].inspection_no+'</td>'+
+                                '<td>PEC'+inspection_data[i].gen_barcode_data[j].poNo+'</td>'+
+                                '<td>'+inspection_data[i].gen_barcode_data[j].jobNo+'</td>'+
+                                '<td>'+inspection_data[i].gen_barcode_data[j].series+'</td>'+
+                                '<td>'+inspection_data[i].warehouse_location+'</td>'+
+                                '<td id="row_amount_'+inspection_data[i].gen_barcode_data[j].ordersheetBatt+'"></td>'+
+                                '<td>'+
+                                    '<input id="row_check_'+inspection_data[i].gen_barcode_data[j].ordersheetBatt+'" type="number" class="col" value="0">'+
+                                '</td>'+
+                            '<tr>'
+
+                        )
+                        
+
+                    }
+                }
+                
+            }
+            function render_predata2(){
+                for(let i = 0; i < batt_data.length; i++){
+                    row_model_count[batt_data[i].ordersheetBatt] = row_model_count[batt_data[i].ordersheetBatt]+1
+                }
+            }
+            function render_predata3(){
+                $.each(row_model_count, function (key, val) {
+                    $('#row_amount_'+key).html(val)
+                });
+            }
             
         }
 
+        function running_number(){
+            let row_number = $('.row_number').length
+            for(let i = 0; i < row_number; i++){
+                $($('.row_number')[i]).html(i+1)
+            }
+        }
 
+        $('#inspection_option').on('change', async function(){
+            await preload()
+            await load_newdata()
+        })
+
+        function load_newdata(){
+            const ins_select = $('#inspection_option').val()
+            const po_select = $('#po_option').val()
+
+            if(ins_select == 0){
+                $('#po_option').attr('disabled',true)
+            }
+            else{
+                $('#po_option').attr('disabled',false)
+
+                $('.table_data').each(function(index){
+                    if(ins_select != $(this).data('ins')){
+                        $(this).remove()
+                    }
+                })
+            }
+            
+        }
     }
 }
 </script>
