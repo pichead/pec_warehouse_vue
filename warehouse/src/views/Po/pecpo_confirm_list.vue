21807b3464af9b5dcba387750bff4af7ba5f8b95
pichead <61868523+pichead@users.noreply.github.com>
2022-06-27 17:08:50 +0700

    Po confirm update
    Po confirm update

--- warehouse/src/views/Po/pecpo_confirm_list.vue
+++ warehouse/src/views/Po/pecpo_confirm_list.vue
@@ -4,15 +4,13 @@
         <div class="container">
             <div class="h3 mt-5 font-weight-bold">รายการ PEC PO</div>
             <div class="row mt-5 mb-4">
-                <div class="col-10">
+                <div class="col-12">
                     <input class="form-control" placeholder="Search" style="background: #f4f4f4;"/>
                 </div>
                 <!-- <div class="col-2 pl-0">
                     <button class="btn btn-outline-danger col" data-toggle="modal" data-target="#createStatus">สร้างสถานะ PEC PO</button>
                 </div> -->
-                <div class="col-2 pl-0">
-                    <a class="btn btn-outline-danger col" href="/createPecpo">Create PO</a>
-                </div>
+
 
                 <div class="modal" id="createStatus" style="display: none;" aria-hidden="true">
                     <div class="modal-dialog">
@@ -105,78 +103,122 @@ export default {
         
     },
     mounted() {
-        
+        render()
+        async function render(){
+            const auth = projectAuth;
+            const user_email = auth.currentUser.email;
+            const get_user_list = await projectFirestore.collection("Users").where("email", '==', user_email).get()
+            const Pos = await projectFirestore.collection("Po").orderBy('createdate','desc').get()
+            var user_permission
+            var role
+            await get_permission()
+            await render_page()
+            await render_po()
 
-        projectFirestore.collection("Po").orderBy('createdate','desc').get().then((Pos) => {
+            function get_permission(){
+                get_user_list.forEach((user)=>{
+                    user_permission = user.data().permission
+                })
+            }
+
+            function render_page(){
+                if(user_permission.includes('confirm_pecpo_manager')){
+                    role = 'manager'
+                }
+                if(user_permission.includes('confirm_pecpo_general_manager')){
+                    role = 'g manager'
+                }
+                
+            }
+
+            function render_po(){
+                $('#polist').html('')
+                Pos.forEach(async function(po){ 
+                    // var creatdate = eval((PmData.data().create_date)*1000);
+                    var crdate = new Date(po.data().createdate*1000);
+                    var crday = crdate.getDate();
+                    var crmonth = crdate.getMonth() + 1;
+                    var cryear = crdate.getFullYear();
+                    if (crday < 10) {
+                        crday = "0" + crday;
+                    }
+                    if (crmonth < 10) {
+                        crmonth = "0" + crmonth;
+                    }
+                    var new_crdate = crday + "-" + crmonth + "-" + cryear;
+                    
+                    const company = await projectFirestore.collection('ContactCompany').doc(po.data().company).get()
+                    const view_btn = get_view_btn()
+                    const approve_status = get_approve_status()
+                    const manager_approve_status = get_manager_approve_status()
+                    const general_mangager_approve_status = get_generalmanager_approve_status()
+
+                    function get_view_btn(){
+                        if(po.data().approve_status == true){
+                            if(role == 'manager'){
+                                return '<button class="btn btn-warning p-1 mr-2 col-5"><a class=" text-white text-decoration-none" href="/viewPecpo/'+po.id+'">View</a></button>'
+                            }
+                            else if(role = 'g manager' &&  po.data().manager_approve_status == true){
+                                return '<button class="btn btn-warning p-1 mr-2 col-5"><a class=" text-white text-decoration-none" href="/viewPecpo/'+po.id+'">View</a></button>'
+                            }
+                            else{
+                                return '<button class="btn btn-warning text-white p-1 mr-2 col-5" disabled>View</button>'
+                            }
+                        }
+                        else{
+                            return '<button class="btn btn-warning text-white p-1 mr-2 col-5" disabled>View</button>'
+                        }
+                    }
+                    function get_approve_status(){
+                        if(po.data().approve_status == true){
+                            return '<div class="col-2 my-auto text-success">'+po.data().approve_status+'</div>'
+                        }
+                        else{
+                            return '<div class="col-2 my-auto text-danger">'+po.data().approve_status+'</div>'
+                        }                                                                                                  
+                    }
+                    function get_manager_approve_status(){
+                        if(po.data().manager_approve_status == true){
+                            return '<div class="col-2 my-auto text-success">'+po.data().manager_approve_status+'</div>'
+                        }
+                        else{
+                            return '<div class="col-2 my-auto text-danger">'+po.data().manager_approve_status+'</div>'
+                        }
+                    }
+                    function get_generalmanager_approve_status(){
+                        if(po.data().generalmanager_approve_status == true){
+                            return '<div class="col-3 my-auto text-success">'+po.data().generalmanager_approve_status+'</div>'
+                        }
+                        else{
+                            return '<div class="col-3 my-auto text-danger">'+po.data().generalmanager_approve_status+'</div>'
+                        }
+                    }
+
+                    $('#polist').append('<div class="row py-2 font-weight-bold border mb-2 rounded text-center"  style="background: #f4f4f4;">'+
+                            '<div class="col my-auto">'+po.data().pecpo_no+'/'+po.data().pecpo_year+'</div>'+
+                            '<div class="col my-auto">'+new_crdate+'</div>'+
+                            approve_status+
+                            manager_approve_status+
+                            general_mangager_approve_status+
+                            '<div class="col-2 d-flex justify-content-center row-hl">'+
+                                view_btn +
+                                '<a class="btn btn-info p-1 col-5" href="/pecpo_print/'+po.id+'">Print</a>'+
+                            '</div>'+
+                        '</div>')
+                })
+            }
+
+
+
+        }
+
+
+        
+        
             
                 
-            $('#polist').html('')
-            Pos.forEach(async function(po){ 
-                // var creatdate = eval((PmData.data().create_date)*1000);
-                var crdate = new Date(po.data().createdate*1000);
-                var crday = crdate.getDate();
-                var crmonth = crdate.getMonth() + 1;
-                var cryear = crdate.getFullYear();
-                if (crday < 10) {
-                    crday = "0" + crday;
-                }
-                if (crmonth < 10) {
-                    crmonth = "0" + crmonth;
-                }
-                var new_crdate = crday + "-" + crmonth + "-" + cryear;
-                
-                const company = await projectFirestore.collection('ContactCompany').doc(po.data().company).get()
-                const view_btn = get_view_btn()
-                const approve_status = get_approve_status()
-                const manager_approve_status = get_manager_approve_status()
-                const general_mangager_approve_status = get_generalmanager_approve_status()
+            
 
-                function get_view_btn(){
-                    if(po.data().approve_status == true){
-                        return '<button class="btn btn-warning p-1 mr-2 col-5"><a class=" text-white text-decoration-none" href="/viewPecpo/'+po.id+'">View</a></button>'
-                    }
-                    else{
-                        return '<button class="btn btn-warning text-white p-1 mr-2 col-5" disabled>View</button>'
-                    }
-                }
-                function get_approve_status(){
-                    if(po.data().approve_status == true){
-                        return '<div class="col-2 my-auto text-success">'+po.data().approve_status+'</div>'
-                    }
-                    else{
-                        return '<div class="col-2 my-auto text-danger">'+po.data().approve_status+'</div>'
-                    }                                                                                                  
-                }
-                function get_manager_approve_status(){
-                    if(po.data().manager_approve_status == true){
-                        return '<div class="col-2 my-auto text-success">'+po.data().manager_approve_status+'</div>'
-                    }
-                    else{
-                        return '<div class="col-2 my-auto text-danger">'+po.data().manager_approve_status+'</div>'
-                    }
-                }
-                function get_generalmanager_approve_status(){
-                    if(po.data().generalmanager_approve_status == true){
-                        return '<div class="col-3 my-auto text-success">'+po.data().generalmanager_approve_status+'</div>'
-                    }
-                    else{
-                        return '<div class="col-3 my-auto text-danger">'+po.data().generalmanager_approve_status+'</div>'
-                    }
-                }
-
-                $('#polist').append('<div class="row py-2 font-weight-bold border mb-2 rounded text-center"  style="background: #f4f4f4;">'+
-                        '<div class="col my-auto">'+po.data().pecpo_no+'/'+po.data().pecpo_year+'</div>'+
-                        '<div class="col my-auto">'+new_crdate+'</div>'+
-                        approve_status+
-                        manager_approve_status+
-                        general_mangager_approve_status+
-                        '<div class="col-2 d-flex justify-content-center row-hl">'+
-                            view_btn +
-                            '<a class="btn btn-info p-1 col-5" href="/pecpo_print/'+po.id+'">Print</a>'+
-                        '</div>'+
-                    '</div>')
-            })
-        })
     }
 }
 </script>
